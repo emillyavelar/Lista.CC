0781bec647699b620b1aff9c1d6f9fc2e655662a
emillyavelar <163145098+emillyavelar@users.noreply.github.com>
2024-09-30 07:46:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function soma(array){
+    let soma = 0;
+    for(let i = 0; i < array.length; i++){
+        soma = soma + array[i];
+    }
+
+    return soma;
+}
+
+function maior(array){
+    if(array.length === 0){
+
+    }
+
+    let maior =  array[0];
+    for(let i = 1)
+
+}

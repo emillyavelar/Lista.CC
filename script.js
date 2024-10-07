f1ddbb69fed3f5c435ceaac5c6f0b8a715b28827
emillyavelar <163145098+emillyavelar@users.noreply.github.com>
2024-10-07 07:50:42 -0300

    salvar

--- script.js
+++ script.js
@@ -13,6 +13,42 @@ function maior(array){
     }
 
     let maior =  array[0];
-    for(let i = 1)
+    for(let i = 1; i < array.length; i++){
+        if(array[i] > maior){
+            maior = array[i];
+        }
+    }
+    return maior;
+}
 
+function pares(array){
+    let pares = [];
+    for(let i = 0; i < array.length; i++){
+        if(array[i] % 2 === 0){
+            pares.push(array[i])
+        }
+    }
+    return pares.length;
+}
+
+function media(array){
+    let soma = 0;
+    for(let i = 0; i < array.length; i++){
+        soma += array[i];
+    }
+    return soma / array.length;
+}
+
+function positivosENegativos(array){
+    let positivos = 0;
+    let negativos = 0;
+    for(let i = 0; i < array.length; i++){
+        if(array[i] >= 0){
+            console.log(array[i] + "positivo");
+        }else{
+
+            console.log(array[i] + "negativo");
+        }
+            
+    }
 }
